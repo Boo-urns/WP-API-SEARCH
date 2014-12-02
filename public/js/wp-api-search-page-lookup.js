9210a8a28dde534208dfa0714b95eec2ec3d5a22
Corey Burns <coreyb@simantel.com>
2014-12-02 14:47:33 -0600

    Removing parent selection function

--- public/js/wp-api-search-page-lookup.js
+++ public/js/wp-api-search-page-lookup.js
@@ -198,20 +198,3 @@ function highlightTerm(content, term) {
 
 	return content;
 }
-
-
-function getSelectionParentElement() {
-    var parentEl = null, sel;
-    if (window.getSelection) {
-        sel = window.getSelection();
-        if (sel.rangeCount) {
-            parentEl = sel.getRangeAt(0).commonAncestorContainer;
-            if (parentEl.nodeType != 1) {
-                parentEl = parentEl.parentNode;
-            }
-        }
-    } else if ( (sel = document.selection) && sel.type != "Control") {
-        parentEl = sel.createRange().parentElement();
-    }
-    return parentEl;
-}
